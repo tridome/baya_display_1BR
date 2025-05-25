2496e3dca683202c8706a69fb5d33ee2ea9b3111
tridome <tech@tridomestructures.com>
2025-05-25 01:25:32 -0400

    things

---
+++ data.js
@@ -0,0 +1,266 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-front",
+      "name": "FRONT",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6427544531135894,
+        "pitch": 0.01394177284004705,
+        "fov": 1.421937074762129
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.553088573412305,
+          "pitch": 0.08405267279727369,
+          "rotation": 0,
+          "target": "1-bedroom--kitchen"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6938612708970817,
+          "pitch": -0.22488109752821828,
+          "title": "HAMMER",
+          "text": "HAMMER"
+        },
+        {
+          "yaw": 1.591075457138012,
+          "pitch": 0.5199615597184284,
+          "title": "Entry",
+          "text": "Even before you step inside, the space asks you to slow down."
+        }
+      ]
+    },
+    {
+      "id": "1-bedroom--kitchen",
+      "name": "BEDROOM & KITCHEN",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.8514013724757206,
+        "pitch": 0.15923878351148701,
+        "fov": 1.421937074762129
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8720809164044745,
+          "pitch": 0.07556724211260502,
+          "rotation": 0,
+          "target": "0-front"
+        },
+        {
+          "yaw": 1.4440084069662422,
+          "pitch": 0.4656601859678293,
+          "rotation": 0,
+          "target": "2-bath"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.9543704437830005,
+          "pitch": -0.3510376445334913,
+          "title": "HVAC - Mini Split in Wall",
+          "text": "WORDS"
+        },
+        {
+          "yaw": 1.892855164534672,
+          "pitch": 0.34073136985787045,
+          "title": "Lumencache DC Lighting",
+          "text": "WORDS"
+        },
+        {
+          "yaw": -1.9151150649841426,
+          "pitch": 0.4181364231399982,
+          "title": "DECK",
+          "text": "WORDS"
+        },
+        {
+          "yaw": -2.9511964616764352,
+          "pitch": -0.13950788537117376,
+          "title": "BACKLIT REMOVABLE LINER",
+          "text": "WORDS"
+        },
+        {
+          "yaw": -0.5190606564215052,
+          "pitch": 0.9173819048748406,
+          "title": "Centering",
+          "text": "Movement is natural here, no corners to navigate, no noise to fight"
+        },
+        {
+          "yaw": 2.7194409971746953,
+          "pitch": 0.5247376232482672,
+          "title": "Take a Break",
+          "text": "What surrounds you reminds you to slow down, and stay a little longer"
+        },
+        {
+          "yaw": 1.6898503808190446,
+          "pitch": 0.027119767180817433,
+          "title": "CLOSET",
+          "text": "WORDS"
+        },
+        {
+          "yaw": 0.7866042579846937,
+          "pitch": 0.39360434400205335,
+          "title": "HAMMER",
+          "text": "WORDS"
+        },
+        {
+          "yaw": 1.1351313479642435,
+          "pitch": 0.19587438210855446,
+          "title": "KITCHENET",
+          "text": "WORDS"
+        },
+        {
+          "yaw": -2.4446364729685364,
+          "pitch": -0.2764337209085852,
+          "title": "HAMMER",
+          "text": "HAMMER"
+        },
+        {
+          "yaw": -1.8471149896855668,
+          "pitch": -0.4722718655755944,
+          "title": "HAMMER&nbsp;",
+          "text": "HAMMER"
+        },
+        {
+          "yaw": -1.4414881248355975,
+          "pitch": 0.19152039880825633,
+          "title": "HAMMER",
+          "text": "HAMMER"
+        },
+        {
+          "yaw": 0.12032345396158917,
+          "pitch": -0.41414266490560614,
+          "title": "HAMMER",
+          "text": "HAMMER"
+        },
+        {
+          "yaw": 1.3040123979879148,
+          "pitch": 1.1321410122024673,
+          "title": "HAMMER",
+          "text": "HAMMER"
+        }
+      ]
+    },
+    {
+      "id": "2-bath",
+      "name": "BATH",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6024313520263291,
+        "pitch": -0.0010779904647790772,
+        "fov": 1.421937074762129
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7977565253729875,
+          "pitch": 0.10672581682180748,
+          "rotation": 0,
+          "target": "1-bedroom--kitchen"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.0124974081069853,
+          "pitch": -0.8451706869145745,
+          "title": "DC vent fan",
+          "text": "WORDS"
+        },
+        {
+          "yaw": 1.1009155986101575,
+          "pitch": 0.3062201882353346,
+          "title": "Wall-Hung, Euro Style Toilet",
+          "text": "WORDS"
+        },
+        {
+          "yaw": 0.9352805985731685,
+          "pitch": 0.9143738751512203,
+          "title": "Waterproof Floor",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.905309855563769,
+          "pitch": -0.1913193734023224,
+          "title": "Rainstick",
+          "text": "WORDS"
+        },
+        {
+          "yaw": 1.4865243665003627,
+          "pitch": 0.10353494911713135,
+          "title": "Wall-Mount Faucet &amp; Stone Sink",
+          "text": "WORDS"
+        },
+        {
+          "yaw": 0.3032833729638966,
+          "pitch": -0.39365186535402863,
+          "title": "BUILD",
+          "text": "WORDS"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
